99e912a4e9ba04c44b464c3d8fa513582933175d
zhejiangxiaomai <358088534@qq.com>
2016-03-28 21:07:53 +0800

    database

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
 # Register your models here.
+from db.models import MemberStatus,Advice,CommissionDetail,CommissionOrder,Member,Message,OrderForm,Product,Service,ServiceAccount,ShortMessage
+
+class MemberStatuss(admin.ModelAdmin):
+    list_display=('status_id','status_desc')
+    search_fields=('status_id','status_desc')
+class Advices(admin.ModelAdmin):
+    list_display=('advice_id','user_name','advice_content','advice_created','reply_time','service')
+    search_fields=('advice_id','user_name','advice_content','advice_created','reply_time','service')
+class CommissionDetails(admin.ModelAdmin):
+    list_display=('commission_type','commission_desc')
+    search_fields=('commission_type','commission_desc')
+class CommissionOrders(admin.ModelAdmin):
+    list_display=('commission_id','user_name','commission_price','commission_type','commission_memo','commission_created',\
+                  'commission_sent','commission_status')
+    search_fields=('commission_id','user_name','commission_price','commission_type','commission_memo','commission_created',\
+                   'commission_sent','commission_status')
+class Members(admin.ModelAdmin):
+    list_display=('user_name','password','nickname','status','service_id','reference_id','user_id','delegation_phone',\
+                  'delegation_info','bind_phone','weixin_id','bank','account','card_holder','receiver',\
+                  'receiver_phone','receiver_addr','register_time','confirm_time')
+    search_fields=('user_name','password','nickname','status','service_id','reference_id','user_id','delegation_phone',\
+                   'delegation_info','bind_phone','weixin_id','bank','account','card_holder','receiver',\
+                   'receiver_phone','receiver_addr','register_time','confirm_time')
+class Messages(admin.ModelAdmin):
+    list_display=('message_id','message_title','message_content','sent_time','message_status')
+    search_fields=('message_id','message_title','message_content','sent_time','message_status')
+class OrderForms(admin.ModelAdmin):
+    list_display=('order_id','order_rank','user_name','order_price','order_type','order_created','order_finished',\
+                  'order_memo','order_status','express_name','express_number')
+    search_fields=('order_id','order_rank','user_name','order_price','order_type','order_created','order_finished',\
+                  'order_memo','order_status','express_name','express_number')
+class Products(admin.ModelAdmin):
+    list_display=('product_id','product_name','product_price')
+    search_fields=('product_id','product_name','product_price')
+class Services(admin.ModelAdmin):
+    list_display=('service_id','service_name','service_pwd','service_area','role')
+    search_fields=('service_id','service_name','service_pwd','service_area','role')
+class ServiceAccounts(admin.ModelAdmin):
+    list_display=('service_id','bank','bank_account','card_holder','phone')
+    search_fields=('service_id','bank','bank_account','card_holder','phone')
+class ShortMessages(admin.ModelAdmin):
+    list_display=('message_id','message_content')
+    search_fields=('message_id','message_content')
+
+admin.site.register(MemberStatus,MemberStatuss)
+admin.site.register(Advice,Advices)
+admin.site.register(CommissionDetail,CommissionDetails)
+admin.site.register(CommissionOrder,CommissionOrders)
+admin.site.register(Member,Members)
+admin.site.register(Message,Messages)
+admin.site.register(OrderForm,OrderForms)
+admin.site.register(Product,Products)
+admin.site.register(Service,Services)
+admin.site.register(ServiceAccount,ServiceAccounts)
+admin.site.register(ShortMessage,ShortMessages)
+
+
--- tlweb/settings.py
+++ tlweb/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'tlweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD':'imdb',
+        'HOST':'172.29.253.152',
+        'PORT':'3306',
     }
 }
 
